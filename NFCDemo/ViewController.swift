cfa4bd6e8df76489fa37a417cb07797abd54f453
douglas_taquary <douglastaquary@gmail.com>
2017-11-25 10:57:58 -0200

    Initial commit

--- NFCDemo/ViewController.swift
+++ NFCDemo/ViewController.swift
@@ -7,19 +7,69 @@
 //
 
 import UIKit
+import CoreNFC
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var payloadDescription: UILabel!
+    
+    var detectedMessages = [NFCNDEFMessage]()
+    var session: NFCNDEFReaderSession?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        payloadDescription.text = "Clique para iniciar a leitura!\n🕵🏻"
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func beginingScan(_ sender: Any) {
+        session = NFCNDEFReaderSession(delegate: self, queue: nil, invalidateAfterFirstRead: true)
+        
+        payloadDescription.text = "Clique para iniciar a leitura!"
+        
+        session?.begin()
     }
+    
+    
+}
 
 
+extension ViewController: NFCNDEFReaderSessionDelegate {
+    
+    //Read Fail
+    
+    func readerSession(_ session: NFCNDEFReaderSession,
+                       didInvalidateWithError error: Error) {
+        
+        // Check invalidation reason from the returned error. A new session instance is required to read new tags.
+        if let readerError = error as? NFCReaderError {
+            // Show alert dialog box when the invalidation reason is not because of a read success from the single tag read mode,
+            // or user cancelled a multi-tag read mode session from the UI or programmatically using the invalidate method call.
+            if (readerError.code != .readerSessionInvalidationErrorFirstNDEFTagRead)
+                && (readerError.code != .readerSessionInvalidationErrorUserCanceled) {
+                let alertController = UIAlertController(title: "Sessão inválida!", message: error.localizedDescription, preferredStyle: .alert)
+                alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                DispatchQueue.main.async {
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
+    
+    // Read Ok
+    
+    func readerSession(_ session: NFCNDEFReaderSession,
+                       didDetectNDEFs messages: [NFCNDEFMessage]) {
+        
+        let infoTag = messages[0]
+        
+        payloadDescription.text = "🎉"
+        print("Payload: \n\(infoTag)")
+    }
+    
 }
 
